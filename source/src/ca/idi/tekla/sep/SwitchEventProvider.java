437a381dc6023febf256a266a70ba607b6d2e42a
vishesh singhal <vishesh.singhal91@gmail.com>
2012-04-03 23:27:18 +0530

    added fullReset timeout preference

--- source/src/ca/idi/tekla/sep/SwitchEventProvider.java
+++ source/src/ca/idi/tekla/sep/SwitchEventProvider.java
@@ -317,12 +317,13 @@ public class SwitchEventProvider extends Service implements Runnable {
 			handleSwitchEvent(switchChanges, mSwitchStates);
 
 			if (mSwitchStates != STATE_DEFAULT) {
+				long fullResetDelay=TeclaApp.persistence.getFullResetTimeout();
 				if (TeclaApp.persistence.isInverseScanningEnabled()) {
 					// FIXME: Temporal beta implementation for specific user (should turn into a preference)
 					// 4 full scans before calling home
 					TeclaApp.getInstance().postDelayedFullReset(32 * TeclaApp.persistence.getScanDelay());
 				} else {
-					TeclaApp.getInstance().postDelayedFullReset(FULL_RESET_TIMEOUT);
+					TeclaApp.getInstance().postDelayedFullReset(fullResetDelay);
 				}
 			}
 			
