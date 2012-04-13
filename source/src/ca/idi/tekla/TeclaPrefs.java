4497dc44484ae716b4ad76c17926d18f57cb5c3d
Ankit Daftery <ankitdaf@gmail.com>
2012-04-13 22:18:45 +0530

    Added notification for manual disconnect of Shield

--- source/src/ca/idi/tekla/TeclaPrefs.java
+++ source/src/ca/idi/tekla/TeclaPrefs.java
@@ -41,6 +41,7 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.text.AutoText;
 import android.util.Log;
+import android.widget.Toast;
 
 public class TeclaPrefs extends PreferenceActivity
 implements SharedPreferences.OnSharedPreferenceChangeListener {
@@ -302,6 +303,7 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 				// switch event provider without breaking
 				// connection with other potential clients.
 				// Should perhaps use Binding?
+				TeclaApp.getInstance().showToast(R.string.shield_connect_cancelled);
 				closeDialog();
 				if (!mPrefFullScreenSwitch.isChecked()) {
 					mPrefSelfScanning.setChecked(false);
