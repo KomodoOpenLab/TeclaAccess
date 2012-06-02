62611dedfd99dc63ff8ec4a66c3ffcc64cb22bae
Elyas BHY <elyas.bhy@gmail.com>
2012-06-02 14:15:07 +0300

    Code refactoring.

--- source/src/ca/idi/tekla/ime/MorseChart.java
+++ source/src/ca/idi/tekla/ime/MorseChart.java
@@ -1,15 +1,12 @@
 package ca.idi.tekla.ime;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
-import ca.idi.tekla.R;
 import ca.idi.tekla.TeclaApp;
 import android.content.Context;
 import android.graphics.Color;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -22,12 +19,7 @@ public class MorseChart {
 	
 
 	public LinearLayout ll;
-	private TableLayout tl1;
-	private TableLayout tl2;
-	private TableLayout tl3;
-	private TableLayout tl4;
-	private TableLayout tl5;
-	private TableLayout tl6;
+	private TableLayout[] tls = new TableLayout[6];
 	private int tableSelector = 0;
 
 	public MorseChart(Context context, TeclaMorse mTeclaMorse) {
@@ -42,30 +34,11 @@ public class MorseChart {
 		ll.setLayoutParams(llParams);
 		ll.setOrientation(LinearLayout.HORIZONTAL);
 		
-		tl1 = new TableLayout(context);
-        tl1.setLayoutParams(tlParams);
-        
-		tl2 = new TableLayout(context);
-        tl2.setLayoutParams(tlParams);
-        
-		tl3= new TableLayout(context);
-        tl3.setLayoutParams(tlParams);		
-        
-        tl4 = new TableLayout(context);
-        tl4.setLayoutParams(tlParams);
-        
-		tl5 = new TableLayout(context);
-        tl5.setLayoutParams(tlParams);
-        
-		tl6= new TableLayout(context);
-        tl6.setLayoutParams(tlParams);
-        
-        ll.addView(tl1, llParams);
-        ll.addView(tl2, llParams);
-        ll.addView(tl3, llParams);
-        ll.addView(tl4, llParams);
-        ll.addView(tl5, llParams);
-        ll.addView(tl6, llParams);
+		for (int i = 0; i < tls.length; i++) {
+			tls[i] = new TableLayout(context);
+	        tls[i].setLayoutParams(tlParams);
+	        ll.addView(tls[i], llParams);
+		}
         
         
         String s = mTeclaMorse.getCurrentChar();
@@ -78,17 +51,17 @@ public class MorseChart {
         	Log.d(TeclaApp.TAG, "Map: " + map.toString());
         	Iterator<Entry<String,String>> it = map.entrySet().iterator();
         	
-        	int i = 0;
+        	int j = 0;
         	while (it.hasNext()) {
     			Entry<String,String> entry = it.next();
     			
         		TableRow tr = new TableRow(context);
-            	tr.setId(100+i);
+            	tr.setId(100+j);
             	tr.setLayoutParams(trParams);
             	tr.setBaselineAligned(true);
             	
             	TextView charTV = new TextView(context);
-            	charTV.setId(200+i);
+            	charTV.setId(200+j);
             	charTV.setText(entry.getValue());
             	charTV.setTextColor(0xFF5887ED);
             	//charTV.setTextColor(Color.WHITE);
@@ -96,14 +69,14 @@ public class MorseChart {
             	tr.addView(charTV);
             	
             	TextView morseTV = new TextView(context);
-            	morseTV.setId(300+i);
+            	morseTV.setId(300+j);
             	morseTV.setText(entry.getKey());
             	morseTV.setTextColor(Color.WHITE);
             	//morseTV.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
             	tr.addView(morseTV);
             	
             	addViewToNextTable(tr, tlParams);
-            	i++;
+            	j++;
             	tableSelector++;
         	}
         	
@@ -112,29 +85,8 @@ public class MorseChart {
 	}
 
 	private void addViewToNextTable(View v, TableLayout.LayoutParams tlParams) {
-		switch (tableSelector % 6) {
-		
-		case 0:
-			tl1.addView(v, tlParams);
-			break;
-		case 1:
-			tl2.addView(v, tlParams);
-			break;
-		case 2:
-			tl3.addView(v, tlParams);
-			break;	
-		case 3:
-			tl4.addView(v, tlParams);
-			break;
-		case 4:
-			tl5.addView(v, tlParams);
-			break;
-		case 5:
-			tl6.addView(v, tlParams);
-			break;
-		default:
-			break;
-		}
+		int index = tableSelector % 6;
+		tls[index].addView(v, tlParams);
 	}
 	
 
