07756260e0dd29a01119f964e5312d14383f5c23
elyas-bhy <elyas.bhy@gmail.com>
2012-07-06 00:09:32 +0200

    Added a preference for Morse switch input method.

--- source/src/ca/idi/tekla/util/Persistence.java
+++ source/src/ca/idi/tekla/util/Persistence.java
@@ -21,6 +21,7 @@ public class Persistence {
 	public static final String PREF_VARIANTS = "variants";
 	public static final String PREF_VARIANTS_KEY = "variants_key";
 	public static final String PREF_MORSE_MODE = "morse_mode";
+	public static final String PREF_MORSE_SWITCH_MODE = "morse_switch_mode";
 	public static final String PREF_PERSISTENT_KEYBOARD = "persistent_keyboard";
 	public static final String PREF_AUTOHIDE_TIMEOUT = "autohide_timeout";
 	
@@ -57,6 +58,7 @@ public class Persistence {
 	public static final String PREF_INVERSE_SCANNING = "inverse_scanning";
 	public static final String PREF_SCAN_DELAY_INT = "scan_delay_int";
 	public static final String PREF_REPEAT_DELAY_INT = "morse_repeat_int";
+	public static final String DEFAULT_MORSE_SWITCH_MODE = "0";
 	public static final long DEFAULT_FULL_RESET_TIMEOUT = 3; //second
 	public static final int DEFAULT_SCAN_DELAY = 1000;
 	public static final int DEFAULT_REPEAT_FREQ = 750;
@@ -227,6 +229,12 @@ public class Persistence {
 
 	public int getRepeatFrequency() {
 		return shared_prefs.getInt(PREF_REPEAT_DELAY_INT, DEFAULT_REPEAT_FREQ);
+	}
+	
+	public int getMorseSwitchMode() {
+		//getInt does not work with ListPreference and string type arrays,
+		//so use getString instead
+		return Integer.parseInt(shared_prefs.getString(PREF_MORSE_SWITCH_MODE, DEFAULT_MORSE_SWITCH_MODE));
 	}
 
 }
