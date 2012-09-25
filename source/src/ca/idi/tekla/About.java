dd9cf23344433c925a0a8e88424ada9c479b6beb
Kelvin Mak <kelvin.mak@ryerson.ca>
2012-09-25 18:05:06 -0400

    Refactor of code
    Removal of unused packages. Addition of comments.

--- source/src/ca/idi/tekla/About.java
+++ source/src/ca/idi/tekla/About.java
@@ -3,12 +3,11 @@ package ca.idi.tekla;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.view.View.OnFocusChangeListener;
 
-public class About extends Activity implements OnFocusChangeListener,
-		OnClickListener {
+public class About extends Activity implements OnFocusChangeListener
+		 {
 
 	TextView title1, label1, title2, label2, title3, label3, title4, label4,
 			title5, label5;
@@ -16,6 +15,7 @@ public class About extends Activity implements OnFocusChangeListener,
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Initialize views of the layout
 		setContentView(R.layout.about);
 		title1 = (TextView) findViewById(R.id.title1);
 		title2 = (TextView) findViewById(R.id.title2);
@@ -28,36 +28,35 @@ public class About extends Activity implements OnFocusChangeListener,
 		label4 = (TextView) findViewById(R.id.label4);
 		label5 = (TextView) findViewById(R.id.label5);
 
+		// Allows for textviews to be clickable
 		title1.setClickable(true);
 		title2.setClickable(true);
 		title3.setClickable(true);
 		title4.setClickable(true);
 		title5.setClickable(true);
 
-		title1.setOnClickListener(this);
-		title2.setOnClickListener(this);
-		title3.setOnClickListener(this);
-		title4.setOnClickListener(this);
-		title5.setOnClickListener(this);
-
+		// Allows for focus to be set on the textview
 		title1.setFocusable(true);
 		title2.setFocusable(true);
 		title3.setFocusable(true);
 		title4.setFocusable(true);
 		title5.setFocusable(true);
 
+		// Allows for focus to be changed if textview is clicked
 		title1.setFocusableInTouchMode(true);
 		title2.setFocusableInTouchMode(true);
 		title3.setFocusableInTouchMode(true);
 		title4.setFocusableInTouchMode(true);
 		title5.setFocusableInTouchMode(true);
 
+		// Sets the listener for a change of focus in each textview
 		title1.setOnFocusChangeListener(this);
 		title2.setOnFocusChangeListener(this);
 		title3.setOnFocusChangeListener(this);
 		title4.setOnFocusChangeListener(this);
 		title5.setOnFocusChangeListener(this);
 
+		// Defaults to hide all the descriptions on start of activity
 		label1.setVisibility(View.GONE);
 		label2.setVisibility(View.GONE);
 		label3.setVisibility(View.GONE);
@@ -67,6 +66,7 @@ public class About extends Activity implements OnFocusChangeListener,
 
 	@Override
 	public void onFocusChange(View view, boolean hasFocus) {
+		// Shows the corrected view and hides all others when focus is changed
 		if (view.equals(title1)) {
 			label1.setVisibility(View.VISIBLE);
 			label2.setVisibility(View.GONE);
@@ -99,21 +99,4 @@ public class About extends Activity implements OnFocusChangeListener,
 			label5.setVisibility(View.VISIBLE);
 		}
 	}
-
-	@Override
-	public void onClick(View view) {
-		if (view.equals(title1)) {
-			title1.requestFocus();
-		} else if (view.equals(title2)) {
-			title2.requestFocus();
-		} else if (view.equals(title3)) {
-			title3.requestFocus();
-		} else if (view.equals(title4)) {
-			title4.requestFocus();
-		} else if (view.equals(title5)) {
-			title5.requestFocus();
-		}
-
-	}
-
 }
