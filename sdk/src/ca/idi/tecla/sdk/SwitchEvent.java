c7cbd7f134c070248a65e5649d84180cd2b8a5f6
Jorge Silva <jsilva@ocad.ca>
2012-12-21 16:18:33 -0500

    Revert "Deleting SDK from main repo"
    This reverts commit 14ed7375ab64e890ec373c95d4477940e3c78240.

---
+++ sdk/src/ca/idi/tecla/sdk/SwitchEvent.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2010-2011, Inclusive Design Research Centre
+ */
+
+package ca.idi.tecla.sdk;
+
+import android.os.Bundle;
+
+public class SwitchEvent {
+	
+	/**
+	 * Intent string used to broadcast switch events. The
+	 * type of event will be packaged as an extra using
+	 * the {@link #EXTRA_SWITCH_CHANGES} and {@link #EXTRA_SWITCH_STATES} extras.
+	 */
+	public static final String ACTION_SWITCH_EVENT_RECEIVED = "ca.idi.tekla.sdk.action.SWITCH_EVENT_RECEIVED";
+	public static final String EXTRA_SWITCH_CHANGES = "ca.idi.tekla.sdk.extra.SWITCH_CHANGES";
+	public static final String EXTRA_SWITCH_STATES = "ca.idi.tekla.sdk.extra.SWITCH_STATES";
+	
+	// MASKS FOR READING SWITCH STATES
+	public static final int SWITCH_J1 = 0x01; //Forward / Up
+	public static final int SWITCH_J2 = 0x02; //Back / Down
+	public static final int SWITCH_J3 = 0x04; //Left
+	public static final int SWITCH_J4 = 0x08; //Right
+	public static final int SWITCH_E1 = 0x10;
+	public static final int SWITCH_E2 = 0x20;
+
+	private int switch_changes, switch_states;
+	
+	public SwitchEvent(Bundle bundle) {
+		switch_changes = bundle.getInt(EXTRA_SWITCH_CHANGES);
+		switch_states = bundle.getInt(EXTRA_SWITCH_STATES);
+	}
+	
+	public int getSwitchChanges() {
+		return switch_changes;
+	}
+	
+	public int getSwitchStates() {
+		return switch_states;
+	}
+	
+	public boolean isPressed(int bitmask) {
+		if ((switch_changes & bitmask) == bitmask) {
+			// Switch changed
+			if ((switch_states & bitmask) != bitmask) {
+				// Switch pressed
+				return true;
+			}
+		}
+		return false;
+	}
+		
+	public boolean isAnyPressed() {
+		if (isPressed(SWITCH_E1)
+				|| isPressed(SWITCH_E2)
+				|| isPressed(SWITCH_J1)
+				|| isPressed(SWITCH_J2)
+				|| isPressed(SWITCH_J3)
+				|| isPressed(SWITCH_J4)
+				) {
+			return true;
+		}
+		return false;
+	}
+		
+	public boolean isReleased(int bitmask) {
+		if ((switch_changes & bitmask) == bitmask) {
+			// Switch changed
+			if ((switch_states & bitmask) == bitmask) {
+				// Switch released
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String toString() {
+		if (switch_changes == SWITCH_J1) return "switch_j1";
+		if (switch_changes == SWITCH_J2) return "switch_j2";
+		if (switch_changes == SWITCH_J3) return "switch_j3";
+		if (switch_changes == SWITCH_J4) return "switch_j4";
+		if (switch_changes == SWITCH_E1) return "switch_e1";
+		if (switch_changes == SWITCH_E2) return "switch_e2";
+		return null;
+	}
+		
+}
