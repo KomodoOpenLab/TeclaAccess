41764b8d01c56ff20c2d50ee349de543dc50e056
Teunissen Enabling <informatie@teunissenenabling.nl>
2013-06-02 15:52:17 +0200

    Very much improved location finder
    Get updated GPS or Network location, if both not avl return old
    location.

--- java/src/ca/idi/tekla/ime/EmergencySMS.java
+++ java/src/ca/idi/tekla/ime/EmergencySMS.java
@@ -18,10 +18,11 @@ package ca.idi.tekla.ime;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Looper;
 import ca.idi.tekla.TeclaApp;
-import ca.idi.tecla.framework.TeclaStatic;
+import ca.idi.tekla.ime.EmergencyLocation.LocationResult;
 import android.telephony.SmsManager;
-import android.location.Criteria;
+import android.util.Log;
 import android.location.Location;
 import android.location.LocationManager;
 
@@ -29,62 +30,57 @@ public class EmergencySMS extends AsyncTask<Object, Void, Boolean> {
 
 	@Override
 	protected Boolean doInBackground(Object... params) {
-		String message = "";
-		Context context = (Context) params[0];
-		String smsNumber = (String) params[1];
-		// with or without emergency GPS setting?
-		if (emergency_GPS_setting(context)) {
-			String location[] = getLocation(context);
-			if (location[0].length() == 0 || location[1].length() == 0) {
-				message = context.getString(ca.idi.tekla.R.string.emergency_SMS_text_withoutLoc);
-				TeclaStatic.logD(TeclaApp.CLASS_TAG, "No location available!");
-			} else {
-				message = context.getString(ca.idi.tekla.R.string.emergency_SMS_text_withLoc) + " http://maps.google.com/maps?&z=17&t=h&q=loc:"
-						+ location[2]
-						+ " Latitude: "
-						+ location[0]
-						+ " Longitude: " + location[1];
-				TeclaStatic.logD(TeclaApp.CLASS_TAG, "Location: " + location[2]);
-			}
-		} else {
-			message = context.getString(ca.idi.tekla.R.string.emergency_SMS_text_withoutLoc);
-		}
-		SmsManager smsManager = SmsManager.getDefault();
-		smsManager.sendTextMessage(smsNumber, null, message, null,
-				null);
+		final Context context = (Context) params[0];
+		final String smsNumber = (String) params[1];
+		final String currentLocation[] = new String[4];
+
+		Looper.prepare();
+		Looper.getMainLooper();	
+
+		LocationResult locationResult = new LocationResult(){
+		    @Override
+		    public void gotLocation(Location location){
+		        //Got the location!
+				Log.d("GPS", "JA! location:" + location);
+				currentLocation[0] = String.valueOf(location.getLatitude());
+				currentLocation[1] = String.valueOf(location.getLongitude());
+				currentLocation[2] = String.valueOf(location.getLatitude()) + "+"
+						+ String.valueOf(location.getLongitude());
+				currentLocation[3] = location.getProvider();
+
+				// start SMS messaging!
+				String message = null;
+				if (emergency_GPS_setting(context)) {
+						message = context.getString(ca.idi.tekla.R.string.emergency_SMS_text_withLoc) + " http://maps.google.com/maps?&z=17&t=h&q=loc:"
+								+ currentLocation[2]
+								+ " Provider: "
+								+ currentLocation[3];
+				} else {
+					message = context.getString(ca.idi.tekla.R.string.emergency_SMS_text_withoutLoc);
+				}
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage(smsNumber, null, message, null, null);
+		    }
+		};
+		
+		EmergencyLocation myLocation = new EmergencyLocation();
+		myLocation.getLocation(context, locationResult); //this
+		
 		// not crashed? we did our job.
 		return true;
 	}
 
 	private boolean emergency_GPS_setting(Context context) {
 		Boolean gpsActive = false;
-		if(TeclaApp.persistence.getEmergencyGPSSetting()) {
+		if (TeclaApp.persistence.getEmergencyGPSSetting()) {
 			LocationManager locMan = (LocationManager) context
 					.getSystemService(Context.LOCATION_SERVICE);
 			// check wether any useable location service is turned on
-			if(locMan.isProviderEnabled(LocationManager.GPS_PROVIDER)) gpsActive = true;
-			if(locMan.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) gpsActive = true;
+			if (locMan.isProviderEnabled(LocationManager.GPS_PROVIDER))
+				gpsActive = true;
+			if (locMan.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+				gpsActive = true;
 		}
 		return gpsActive;
-	}
-
-	private String[] getLocation(Context context) {
-		/* array location:
-		 * [0] Longitude
-		 * [1] Latitude
-		 * [2] after each other with + between for google maps
-		 */
-		String currentLocation[] = new String[3];
-		Criteria criteria = new Criteria();
-		Location location;
-		LocationManager locMan = (LocationManager) context
-				.getSystemService(Context.LOCATION_SERVICE);
-		String currentPosition = locMan.getBestProvider(criteria, false);
-		location = locMan.getLastKnownLocation(currentPosition);
-		currentLocation[0] = String.valueOf(location.getLatitude());
-		currentLocation[1] = String.valueOf(location.getLongitude());
-		currentLocation[2] = String.valueOf(location.getLatitude()) + "+"
-				+ String.valueOf(location.getLongitude());
-		return currentLocation;
 	}
 }
